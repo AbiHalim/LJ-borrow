ce03f0549d4e06c5f9cb30a5c0c7477f72412b2e
AbiHalim <143999006+AbiHalim@users.noreply.github.com>
2024-05-29 08:52:35 +0700

    Delete User.py

--- User.py
+++
@@ -1,6 +0,0 @@
-class User:
-    def __init__(self, UUID, username, email, password_hash):
-        self.UUID = UUID
-        self.username = username
-        self.email = email
-        self.password_hash = password_hash
