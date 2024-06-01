243f94175b0427ccc7a02469e3817bb25d86da49
AbiHalim <e1304457@u.nus.edu>
2024-06-01 15:44:38 +0700

    Fixed log-in button

--- LJ Borrow/LJ Borrow/ContentView.swift
+++ LJ Borrow/LJ Borrow/ContentView.swift
@@ -83,57 +83,56 @@ struct ContentView: View {
                             .padding(.vertical, -50)
                             .font(.system(size: 20, weight: .semibold, design: .rounded))
                         Text("to your Debtor")
-
                             .padding(.vertical, -35)
-                                                        .font(.system(size: 20, weight: .semibold, design: .rounded))
-                                                    Image("LJ Borrow Arrow Bubble")
-                                                        .imageScale(.small)
-                                                        .scaleEffect(0.3)
-                                                        .padding(.vertical, -100)
-                                                    Text("Swipe to continue reading")
-                                                        .padding(.top, -25)
-                                                        .font(.system(size: 14, weight: .regular, design: .rounded))
-                                                }
-                                                    .tag(4)
-                                                VStack {
-                                                    Image("LJ Borrow Logo V2.1")
-                                                        .scaleEffect(0.9)
-                                                        .padding(.bottom, -35)
-                                                    Text("LJ Borrow")
-                                                        .font(.system(size: 30, weight: .bold, design: .rounded))
-                                                    Text("Record Your Debts.")
-                                                        .font(.system(size: 20, weight: .semibold, design: .rounded))
-                                                        .padding(.top, 1)
-                                                    Text("Secure Your Friendship.")
-                                                            .font(.system(size: 20, weight: .semibold, design: .rounded))
-                                                }
-                                                    .tag(5)
-                                            }
-                                            .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                                                .background(Color.clear)
-                                            TextField("Email Address or ID", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                                                .padding()
-                                                .background(Color.white)
-                                                .overlay(RoundedRectangle(cornerRadius: 5)
-                                                    .stroke(Color.gray,
-                                                            lineWidth: 1.5))
-                                                .frame(width: 275, height: 40)
-                                                .padding()
-                                            Button("Sign In") {
-                                                /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
-                                            }
-                                            .padding()
-                                            .background(Color.gray)
-                                            .foregroundColor(.white)
-                                            .overlay(RoundedRectangle(cornerRadius: 5)
-                                                .stroke(Color.gray,
-                                                        lineWidth: 1.5))
-                                            .frame(width: 275, height: 40)
-                                        }
-                                        .padding(.bottom, 200)
-                                    }
-                                }
-                            }
-                            #Preview {
-                                ContentView()
-                            }
+                            .font(.system(size: 20, weight: .semibold, design: .rounded))
+                        Image("LJ Borrow Arrow Bubble")
+                            .imageScale(.small)
+                            .scaleEffect(0.3)
+                            .padding(.vertical, -100)
+                        Text("Swipe to continue reading")
+                            .padding(.top, -25)
+                            .font(.system(size: 14, weight: .regular, design: .rounded))
+                    }
+                        .tag(4)
+                    VStack {
+                        Image("LJ Borrow Logo V2.1")
+                            .scaleEffect(0.9)
+                            .padding(.bottom, -35)
+                        Text("LJ Borrow")
+                            .font(.system(size: 30, weight: .bold, design: .rounded))
+                        Text("Record Your Debts.")
+                            .font(.system(size: 20, weight: .semibold, design: .rounded))
+                            .padding(.top, 1)
+                        Text("Secure Your Friendship.")
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                    }
+                        .tag(5)
+                }
+                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                    .background(Color.clear)
+                TextField("Email Address or ID", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .background(Color.white)
+                    .overlay(RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray,
+                                lineWidth: 1.5))
+                    .frame(width: 275, height: 40)
+                    .padding()
+                Button("Sign In") {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .padding()
+                .background(Color.gray)
+                .foregroundColor(.white)
+                .overlay(RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.gray,
+                            lineWidth: 1.5))
+                .frame(width: 275, height: 40)
+            }
+            .padding(.bottom, 200)
+        }
+    }
+}
+#Preview {
+    ContentView()
+}
