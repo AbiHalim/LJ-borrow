12a859bf537af2a7bc8683a3ab7e2931ee704d31
AbiHalim <e1304457@u.nus.edu>
2024-06-01 14:48:00 +0700

    Create Front End

---
+++ LJ Borrow/LJ Borrow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LJ Borrow
+//
+//  Created by Abi on 01/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LJ Borrow/LJ Borrow/LJ_BorrowApp.swift
@@ -0,0 +1,17 @@
+//
+//  LJ_BorrowApp.swift
+//  LJ Borrow
+//
+//  Created by Abi on 01/06/24.
+//
+
+import SwiftUI
+
+@main
+struct LJ_BorrowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
