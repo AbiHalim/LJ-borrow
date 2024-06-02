4bf9a7a0c42bdb85ed96122f41dec7a197847db3
AbiHalim <e1304457@u.nus.edu>
2024-06-02 16:08:57 +0700

    Added account registration page (Ryan)

--- LJ Borrow/LJ Borrow/ContentView.swift
+++ LJ Borrow/LJ Borrow/ContentView.swift
@@ -9,137 +9,135 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        ZStack {
-            Image("LJ Borrow login page background")
-                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            VStack {
-                TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
-                    VStack {
-                        Image("LJ Borrow Login Page Cartoon 1")
-                            .imageScale(.small)
-                            .scaleEffect(0.3)
-                            .padding(.vertical, -125)
-                        Text("Record and Confirm")
-                            .padding(.vertical, -50)
-                            .font(.system(size: 20, weight: .semibold, design: .rounded))
-                        Text("your Transactions")
-                            .padding(.vertical, -35)
-                            .font(.system(size: 20, weight: .semibold, design: .rounded))
-                        Image("LJ Borrow Arrow Bubble")
-                            .imageScale(.small)
-                            .scaleEffect(0.3)
-                            .padding(.vertical, -100)
-                        Text("Swipe to continue reading")
-                            .padding(.top, -25)
-                            .font(.system(size: 14, weight: .regular, design: .rounded))
-                    }
-                        .tag(1)
-                    VStack {
-                        Image("LJ Borrow Login Page Cartoon 2")
-                            .imageScale(.small)
-                            .scaleEffect(0.3)
-                            .padding(.vertical, -125)
-                        Text("Track Money")
-                            .padding(.vertical, -50)
-                            .font(.system(size: 20, weight: .semibold, design: .rounded))
-                        Text("Borrowed and Lent")
-                            .padding(.vertical, -35)
-                            .font(.system(size: 20, weight: .semibold, design: .rounded))
-                        Image("LJ Borrow Arrow Bubble")
-                            .imageScale(.small)
-                            .scaleEffect(0.3)
-                            .padding(.vertical, -100)
-                        Text("Swipe to continue reading")
-                            .padding(.top, -25)
-                            .font(.system(size: 14, weight: .regular, design: .rounded))
-                    }
-                        .tag(2)
-                    VStack {
-                        Image("LJ Borrow Login Page Cartoon 3")
-                            .imageScale(.small)
-                            .scaleEffect(0.3)
-                            .padding(.vertical, -125)
-                        Text("Get Reminders")
-                            .padding(.vertical, -50)
-                            .font(.system(size: 20, weight: .semibold, design: .rounded))
-                        Text("for your Debts")
-                            .padding(.vertical, -35)
-                            .font(.system(size: 20, weight: .semibold, design: .rounded))
-                        Image("LJ Borrow Arrow Bubble")
-                            .imageScale(.small)
-                            .scaleEffect(0.3)
-                            .padding(.vertical, -100)
-                        Text("Swipe to continue reading")
-                            .padding(.top, -25)
-                            .font(.system(size: 14, weight: .regular, design: .rounded))
-                    }
-                        .tag(3)
-                    VStack {
-                        Image("LJ Borrow Login Page Cartoon 4")
-                            .imageScale(.small)
-                            .scaleEffect(0.3)
-                            .padding(.vertical, -125)
-                        Text("Send Reminders")
-                            .padding(.vertical, -50)
-                            .font(.system(size: 20, weight: .semibold, design: .rounded))
-                        Text("to your Debtor")
-                            .padding(.vertical, -35)
-                            .font(.system(size: 20, weight: .semibold, design: .rounded))
-                        Image("LJ Borrow Arrow Bubble")
-                            .imageScale(.small)
-                            .scaleEffect(0.3)
-                            .padding(.vertical, -100)
-                        Text("Swipe to continue reading")
-                            .padding(.top, -25)
-                            .font(.system(size: 14, weight: .regular, design: .rounded))
-                    }
-                        .tag(4)
-                    VStack {
-                        Image("LJ Borrow Logo V2.1")
-                            .scaleEffect(0.9)
-                            .padding(.bottom, -35)
-                        Text("LJ Borrow")
-                            .font(.system(size: 30, weight: .bold, design: .rounded))
-                        Text("Record Your Debts.")
-                            .font(.system(size: 20, weight: .semibold, design: .rounded))
-                            .padding(.top, 1)
-                        Text("Secure Your Friendship.")
+        NavigationStack {
+            ZStack {
+                Image("LJ Borrow login page background")
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                VStack {
+                    TabView(selection: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Selection@*/.constant(1)/*@END_MENU_TOKEN@*/) {
+                        VStack {
+                            Image("LJ Borrow Login Page Cartoon 1")
+                                .scaleEffect(0.3)
+                                .padding(.top, 380)
+                                .padding(.bottom, -175)
+                            Text("Record and Confirm")
                                 .font(.system(size: 20, weight: .semibold, design: .rounded))
-                    }
+                            Text("your Transactions")
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                                .padding(.bottom, -100)
+                            Image("LJ Borrow Arrow Bubble V2.0")
+                                .imageScale(.small)
+                                .scaleEffect(0.6)
+                                .padding(.top, -50)
+                                .padding(.bottom, 400)
+                        }
+                        .tag(1)
+                        VStack {
+                            Image("LJ Borrow Login Page Cartoon 2")
+                                .scaleEffect(0.3)
+                                .padding(.top, 380)
+                                .padding(.bottom, -175)
+                            Text("Track Money")
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                            Text("Borrowed and Lent")
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                                .padding(.bottom, -100)
+                            Image("LJ Borrow Arrow Bubble V2.0")
+                                .imageScale(.small)
+                                .scaleEffect(0.6)
+                                .padding(.top, -50)
+                                .padding(.bottom, 400)
+                        }
+                        .tag(2)
+                        VStack {
+                            Image("LJ Borrow Login Page Cartoon 3")
+                                .scaleEffect(0.3)
+                                .padding(.top, 380)
+                                .padding(.bottom, -175)
+                            Text("Get Reminders")
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                            Text("for your Debts")
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                                .padding(.bottom, -100)
+                            Image("LJ Borrow Arrow Bubble V2.0")
+                                .imageScale(.small)
+                                .scaleEffect(0.6)
+                                .padding(.top, -50)
+                                .padding(.bottom, 400)
+                        }
+                        .tag(3)
+                        VStack {
+                            Image("LJ Borrow Login Page Cartoon 4")
+                                .scaleEffect(0.3)
+                                .padding(.top, 380)
+                                .padding(.bottom, -175)
+                            Text("Send Reminders")
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                            Text("to your Debtor")
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                                .padding(.bottom, -100)
+                            Image("LJ Borrow Arrow Bubble V2.0")
+                                .imageScale(.small)
+                                .scaleEffect(0.6)
+                                .padding(.top, -50)
+                                .padding(.bottom, 400)
+                        }
+                        .tag(4)
+                        VStack {
+                            Image("LJ Borrow Logo V2.1")
+                                .scaleEffect(0.9)
+                                .padding(.bottom, -35)
+                            Text("LJ Borrow")
+                                .font(.system(size: 30, weight: .bold, design: .rounded))
+                            Text("Record Your Debts.")
+                                .font(.system(size: 20, weight:
+                                        .semibold, design: .rounded))
+                                .padding(.top, 1)
+                            Text("Secure Your Friendship.")
+                                .font(.system(size: 20, weight: .semibold, design: .rounded))
+                        }
                         .tag(5)
-                }
-                .tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
-                    .background(Color.clear)
-                TextField("Email Address or ID", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    }.tabViewStyle(PageTabViewStyle(indexDisplayMode: .never))
+                        .background(Color.clear)
+                    TextField("Email Address or Username", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .padding()
+                        .background(Color.white)
+                        .overlay(RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.gray, lineWidth: 1.5))
+                        .frame(width: 275, height: 40)
+                    TextField("Password", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                        .padding()
+                        .background(Color.white)
+                        .overlay(RoundedRectangle(cornerRadius: 5)
+                            .stroke(Color.gray, lineWidth: 1.5))
+                        .frame(width: 275, height: 40)
+                        .padding()
+                    Button("Sign In")    {
+                        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    }
                     .padding()
-                    .background(Color.white)
+                    .background(Color.gray)
+                    .foregroundColor(.white)
                     .overlay(RoundedRectangle(cornerRadius: 5)
-                        .stroke(Color.gray,
-                                lineWidth: 1.5))
+                        .stroke(Color.gray, lineWidth: 1.5))
                     .frame(width: 275, height: 40)
-                TextField("Password", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
-                    .padding()
-                    .background(Color.white)
-                    .overlay(RoundedRectangle(cornerRadius: 5)
-                        .stroke(Color.gray,
-                                lineWidth: 1.5))
-                    .frame(width: 275, height: 40)
-                    .padding()
-
-                Button("Sign In") {
-                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                    NavigationLink(destination: SecondView()) {
+                        Text("Don't have an account? Register now")
+                            .foregroundColor(.black)
+                            .padding(.top, 5)
+                            .underline()
+                            .navigationBarBackButtonHidden(true)
+                    }
                 }
-                .padding()
-                .background(Color.gray)
-                .foregroundColor(.white)
-                .overlay(RoundedRectangle(cornerRadius: 5)
-                    .stroke(Color.gray,lineWidth: 1.5))
-                .frame(width: 275, height: 40)
+                .padding(.bottom, 160)
             }
-            .padding(.bottom, 175)
+            .buttonStyle(PlainButtonStyle())
         }
     }
 }
-#Preview {
-    ContentView()
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
 }
---
+++ LJ Borrow/LJ Borrow/SecondView.swift
@@ -0,0 +1,71 @@
+//
+//  SecondView.swift
+//  LJ Borrow
+//
+//  Created by Abi on 02/06/24.
+//
+
+import SwiftUI
+
+struct SecondView: View {
+    var body: some View {
+        ZStack{
+            Image("LJ Borrow login page background")
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            VStack{
+                Image("LJ Borrow Register Page Graphics V1.2")
+                    .scaleEffect(0.7)
+                    .padding(.bottom, -75)
+                TextField("Username", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .background(Color.white)
+                    .overlay(RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray, lineWidth: 1.5))
+                    .frame(width: 275, height: 40)
+                TextField("Email", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .background(Color.white)
+                    .overlay(RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray, lineWidth: 1.5))
+                    .frame(width: 275, height: 40)
+                    .padding()
+                TextField("Password", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .background(Color.white)
+                    .overlay(RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray, lineWidth: 1.5))
+                    .frame(width: 275, height: 40)
+                TextField("Re-enter Password", text: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant("")/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .background(Color.white)
+                    .overlay(RoundedRectangle(cornerRadius: 5)
+                        .stroke(Color.gray, lineWidth: 1.5))
+                    .frame(width: 275, height: 40)
+                    .padding()
+                Button("Register")    {
+                    /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Action@*/ /*@END_MENU_TOKEN@*/
+                }
+                .padding()
+                .background(Color.gray)
+                .foregroundColor(.white)
+                .overlay(RoundedRectangle(cornerRadius: 5)
+                    .stroke(Color.gray, lineWidth: 1.5))
+                .frame(width: 275, height: 40)
+                NavigationLink(destination: ContentView()) {
+                    Text("Already have an account? Sign in now")
+                        .foregroundColor(.black)
+                        .padding(.top, 5)
+                        .underline()
+                        .navigationBarBackButtonHidden(true)
+                }
+            }
+            .padding(.bottom, 125)
+        }
+    }
+}
+    
+struct SecondView_Previews: PreviewProvider {
+    static var previews: some View {
+        SecondView()
+    }
+}
